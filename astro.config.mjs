f08c6cf4fd5494ad51f4668b9ef13a05f47efd11
Rene Kaesler <renekaesler@gmail.com>
2024-06-04 00:54:29 +0200

    docs: use starlight theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,35 @@
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://xylit.dev',
+  integrations: [
+    starlight({
+      favicon: '/logo.svg',
+      title: 'Xylit',
+      editLink: {
+        baseUrl: 'https://github.com/xylitdev/xylitdev.github.io/edit/main/',
+      },
+      logo: {
+        src: './public/logo.svg'
+      },
+      social: {
+        github: 'https://github.com/xylitdev',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/guides/example/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+});
